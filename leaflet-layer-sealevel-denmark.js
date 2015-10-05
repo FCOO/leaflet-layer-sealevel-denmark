505b5df38e1e5919c601100b844d79f1fdd6f0a0
Jesper Baasch-Larsen <jla@fcoo.dk>
2015-10-05 21:03:25 +0200

    restructured to use click handler

--- leaflet-layer-sealevel-denmark.js
+++ leaflet-layer-sealevel-denmark.js
@@ -9,21 +9,27 @@
             //url: '../bower_components/leaflet-layer-sealevel-denmark/sealevel_stations_denmark.json',
             url: '../json/sealevel_stations_denmark.json',
             onEachFeature: function (feature, layer) {
-                var container = $('<div/>');
-                var headerTemplate = '<h3 class="leaflet-layer-sealevel-denmark-header">${name} - ${position}</h3>';
-                var position = '${latitude}, ${longitude}';
-                var lng = feature.geometry.coordinates[0];
-                var lat = feature.geometry.coordinates[1];
-                position = position.replace('${longitude}', latlngFormat.asTextLng(lng));
-                position = position.replace('${latitude}', latlngFormat.asTextLat(lat));
-                var header = headerTemplate.replace('${name}', feature.properties.nameDK);
-                header = header.replace('${position}', position);
-                container.append($(header));
-                var linkTemplate = location.protocol + "//chart.fcoo.dk/station_timeseries.asp?s=:003${stationId}:046SeaLvl:002DK:001DEFAULT:04d620:04e400:04f0:04a1:04b48:04i0:04c1:04g0:0641:05opopup";
-                var link = linkTemplate.replace('${stationId}', feature.properties.id);
-                var img = $('<img src="' + link + '" height="400" width="620" />');
-                container.append(img);
-                layer.bindPopup(container.html(), {maxWidth: 700, maxHeight: 600});
+                // Use click handler for substituting timezone information
+                layer.on({
+                    click: function (evt) {
+                        var container = $('<div/>');
+                        var headerTemplate = '<h3 class="leaflet-layer-sealevel-denmark-header">${name} - ${position}</h3>';
+                        var position = '${latitude}, ${longitude}';
+                        var lng = feature.geometry.coordinates[0];
+                        var lat = feature.geometry.coordinates[1];
+                        position = position.replace('${longitude}', latlngFormat.asTextLng(lng));
+                        position = position.replace('${latitude}', latlngFormat.asTextLat(lat));
+                        var header = headerTemplate.replace('${name}', feature.properties.nameDK);
+                        header = header.replace('${position}', position);
+                        container.append($(header));
+                        var linkTemplate = location.protocol + "//chart.fcoo.dk/station_timeseries.asp?s=:003${stationId}:046SeaLvl:002DK:001DEFAULT:04d620:04e400:04f0:04a1:04b48:04i0:04c1:04g0:0641:05opopup";
+                        var link = linkTemplate.replace('${stationId}', feature.properties.id);
+                        var img = $('<img src="' + link + '" height="400" width="620" />');
+                        container.append(img);
+                        layer._map.openPopup(container.html(), evt.latlng, {maxWidth: 700, maxHeight: 600});
+                    }
+                });
+
             },
             pointToLayer: function (feature, latlng) {
                 return L.circleMarker(latlng, {
